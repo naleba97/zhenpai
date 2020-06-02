687501a35d2740e9b91438b0451bf864c56abb46
tsukihi <uncreativename0@gmail.com>
2020-06-02 08:14:32 -0500

    indention

--- cogs/tagging/kvstore.py
+++ cogs/tagging/kvstore.py
@@ -1,28 +1,28 @@
 import typing
 
 class TaggingItem(object):
-	"""
-	Thing to be retrieved, can be an image file, a gif, a soundbite, 
-	a url with an embed preview, a youtube link, a text document, etc.
-	"""
-	pass
+    """
+    Thing to be retrieved, can be an image file, a gif, a soundbite, 
+    a url with an embed preview, a youtube link, a text document, etc.
+    """
+    pass
 
 class BaseKeyValueStore(object):
-	def get(self, key: str) -> TaggingItem:
-		raise NotImplementedError
+    def get(self, key: str) -> TaggingItem:
+        raise NotImplementedError
 
-	def get_all(self) -> Generator[TaggingItem, None, None]: # I'm pretty sure this is just __iter__()
-		raise NotImplementedError
+    def get_all(self) -> Generator[TaggingItem, None, None]: # I'm pretty sure this is just __iter__()
+        raise NotImplementedError
 
-	def __contains__(self, key: str):
-		raise NotImplementedError
+    def __contains__(self, key: str):
+        raise NotImplementedError
 
 class DictKeyValueStore(BaseKeyValueStore):
-	def __init__(self):
-		self.kvstore = {} # TODO: load from a pickle file, not sure when we save though
+    def __init__(self):
+        self.kvstore = {} # TODO: load from a pickle file, not sure when we save though
 
-	def get(self, key: str) -> TaggingItem:
-		return self.kvstore.get(key)
+    def get(self, key: str) -> TaggingItem:
+        return self.kvstore.get(key)
 
-	def __contains__(self, key: str):
-		return key in self.kvstore
+    def __contains__(self, key: str):
+        return key in self.kvstore
